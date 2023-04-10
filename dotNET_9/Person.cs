6b8fc203ed1f53b4bd5977fb365c1389cc955ec8
VasyaTarn <126779570+VasyaTarn@users.noreply.github.com>
2023-04-10 22:58:32 +0300

    Add files via upload

---
+++ dotNET_9/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNET_9
+{
+    class Person
+    {
+        private int age;
+        private int height;
+        private int weight;
+        private string eyeColor;
+
+        private string[] colors = {"Black", "Red", "Yellow", "Green", "Blue"};
+
+        public Person()
+        {
+            Random rand = new Random();
+
+            this.age = rand.Next(18, 25);
+            this.height = rand.Next(160, 200);
+            this.weight = rand.Next(50, 120);
+            this.eyeColor = colors[rand.Next(0, colors.Length)];
+        }
+
+        public int Age
+        {
+            get; set;
+        }
+        public int Height
+        {
+            get; set;
+        }
+        public int Weight
+        {
+            get; set;
+        }
+        public string EyeColor
+        {
+            get; set;
+        }
+
+        public void PrintPersonInfo()
+        {
+            Console.WriteLine($"Age:{age}");
+            Console.WriteLine($"Height:{height}");
+            Console.WriteLine($"Weight:{weight}");
+            Console.WriteLine($"Eye color:{eyeColor}");
+        }
+    }
+}
+
